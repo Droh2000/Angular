8bdab7a3c11f12d8604ec95a0abf31ee81b6f5be
diego ortega <diego-chido2011@hotmail.com>
2025-03-08 15:11:50 -0600

    Señales

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,6 +1,6 @@
 // El nombre de agreagr ".component" es una nomenclatura recomendada por Angular
 
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 // Esto por defecto es una simple clase pero para que se combierta en un componente que podamos mostrarlo y renderizarlo
 // Le colocamos el decorador @Component, estos por los menos requieren una pieza (Esto ya se nos indica en el mensaje del error)
@@ -23,14 +23,29 @@ export class CounterPageComponent { // El nombre de la clase debe ser igual al n
     // Queremos mostrar el valor de un contador
     counter = 10;
 
+    // Creamos una señal del counter y establecemos el valor inicial de la senal
+    // Al poner el cursor encima de la variable veremos que dice "WritableSignal" porque es una señal que se pueden escribir
+    // ya que hay otras que no que solo son de solo lectura
+    counterSignal = signal(10);
+
     // Metodo para incrementar el valor del contador, recibimos el valor por el cual queremos incrementarlo
     increaseBy(value: number){
         // Hacemos referencia a la misma propiedad
         this.counter += value;
+
+        // Actualizacion del valor de un Signal basada en un valor anterior
+        // this.counterSignal.set(this.counterSignal() + value); // Del valor que tenga le sumamos el nuevo valor, pero esta sintaxis es dificil de entender
+        // Con esta funcion podemos mandar un callback function que por su inicializacion espera un numero y regresa un numero (Esto se recomienda que hagamos)
+        this.counterSignal.update( (current) => current + value );
     }
 
     resetCounter(){
-        this.counter = 10;
+        this.counter = 0;
+        // Para actualizar el valor de una Signal hay varias formas, esta es una
+        this.counterSignal.set(0);
+        // Ahora cuando tenemos una actualizacion en el valor de una Signal pero dependen del valor anterior de la Signal
+        // se recomienda que hagamos un Update no un SET porque el SET ignora cualquier valor que tenga y solo pone el nuevo valor
+        // pero cuando dependemos del valor anteiror hay varias formas de hacerlo
     }
 }
 
