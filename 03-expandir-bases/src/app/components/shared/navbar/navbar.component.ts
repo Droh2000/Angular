321825395b801c30fa8e574e75bf10e4970b0f27
diego ortega <diego-chido2011@hotmail.com>
2025-03-13 21:08:50 -0600

    RoteLink Active (Mostrar cual opcion del navbar esta Activo)

--- 03-expandir-bases/src/app/components/shared/navbar/navbar.component.ts
+++ 03-expandir-bases/src/app/components/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar', // Esto nos permite poder usarlo en otros componentes
@@ -7,7 +7,10 @@ import { RouterLink } from '@angular/router';
     // Lo importamos para poder usar el enrutamiento (Esta es nuestra primera directiva que sirven para cambiar 
     // el funcionamiento de algo) en este caso lo que queremos hacer es que los <a></a> conoscan la directiva
     // del routerLink (Es donde le pasamos el enlace)
-    RouterLink
+    RouterLink,
+    
+    // Esta directiva es para saber a cual de las opciones del menu tenemos seleccianada
+    RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
 })
