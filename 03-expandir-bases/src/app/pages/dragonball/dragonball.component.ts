eb1cdaf56c789dcf00f641d4c03dd7a8b26a2f18
diego ortega <diego-chido2011@hotmail.com>
2025-03-16 12:49:04 -0600

    @for - Realizar iteraciones de elementos

--- 03-expandir-bases/src/app/pages/dragonball/dragonball.component.ts
+++ 03-expandir-bases/src/app/pages/dragonball/dragonball.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+// Para especificar el tipo de dato del arreglo
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball',
@@ -6,5 +13,13 @@ import { Component } from '@angular/core';
   templateUrl: './dragonball.component.html',
 })
 export class DragonballComponent {
-  
+  // Queremos iterar los elementos de la lista por la cantidad de personajes que nosotros tengamos almacenados
+  // Nos vamos a crear un arreglo de personajes llamaodo "charcter" (Que sera una señal), como al crear arreglos
+  // Angular nos pone el tipo de dato "NEVER" nos creamos una interfaz para ponerle el tipo
+  characters = signal<Character[]>([
+    // Inicializamos el arreglo (Estos datos son los que mostraremos en la lista del HTML)
+    { id: 1, name: 'Goku', power: 9001},
+    { id: 2, name: 'Vegeta', power: 8000},
+    { id: 3, name: 'Piccolo', power: 3000}
+  ]);
 }
