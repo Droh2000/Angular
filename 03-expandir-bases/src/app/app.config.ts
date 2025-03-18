62281c3f5e087589048a54e86e5f02688f374173
diego ortega <diego-chido2011@hotmail.com>
2025-03-17 21:17:50 -0600

    Despliegue y HashRouter

--- 03-expandir-bases/src/app/app.config.ts
+++ 03-expandir-bases/src/app/app.config.ts
@@ -1,11 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     
     provideZoneChangeDetection({ eventCoalescing: true }), 
     
-    provideRouter(routes)]
+    provideRouter(routes),
+
+    // Esta configuracion es para lo sitios donde tenemos que implementar el HashStrategy
+    // El cual es usado en los SPA
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
+    // Despues de implementar esto en nuestra aplicacion veremos que la URL cambio y nos pone un #
+    // ese es como un indicador que evita que el navegador redireccione a esa carpeta, siempre estamos en el ROOT
+    // Asi: ROOT/#/pagina
+    // Despues de esta modificacion volvemos a ejectuar el comando: ng build
+  ],
 };
