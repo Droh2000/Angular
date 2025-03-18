d8f7649d29d4131a0009ac3f36aa48946cd4b8bf
diego ortega <diego-chido2011@hotmail.com>
2025-03-17 18:56:02 -0600

    Efectos y LocalStorage para guardar informacion

--- 03-expandir-bases/src/app/service/dragonball.service.ts
+++ 03-expandir-bases/src/app/service/dragonball.service.ts
@@ -1,5 +1,5 @@
 // aservice+tab para crear automaticamente
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 /*
@@ -24,10 +24,43 @@ export class DragonballService {
         { id: 2, name: 'Vegeta', power: 8000},
       ]);
     
-      // Esto es lo que vamos a mandar a llamar y para insertarlo
-      addCharacter(character: Character){
-        // Aqui insertamos el personaje (Para actualizar el signal usamos update)
-        this.characters.update((list) => [...list, character]);
-    
-      }
+    // Esto es lo que vamos a mandar a llamar y para insertarlo
+    addCharacter(character: Character){
+    // Aqui insertamos el personaje (Para actualizar el signal usamos update)
+    this.characters.update((list) => [...list, character]);
+
+    }
+
+    /*
+        Vamos a hacer persistente la informacion para que al actualizar el navegador no se piedra la informacion
+        En los navegadores tenemos el apartado del
+            * Local Storage: que tiene un espacio del navegador y aparte tiene un espacio por sitio web
+            * Session Storage
+            * Cookiees
+            
+        El Local y el Session funcion igual en el que tenemos que guardar Key/Value (El value tiene que ser String a fuerzas)
+        con el Session cada vez que cerremos todo el navegador se va a borrar lo que este almacenado y en el Local Storage se
+        va a seguir perservando a pesar que salgamos del navegador y se reinicie la computadora (Mos o menos es igual a las 
+        cookies solo que estas tiene menos espacios)
+
+        Para grabar en el Local Storage nos vamos al servicio y vamos a usar una nueva funcion que son los Efectos, estos nos 
+        sirven para poder ejecutar o disparar una accion secundaria. Le mandamos un callback que se va a disparar cada vez
+        que una accion suceda (En este caso cada vez que los chracters cambien guarde en el Localstorage)
+
+        Esto lo podemos implementar en el constructor pero lo mas comun es hacerlo asi:
+        Poniendo un nombre descriptivo de lo que hace el efecto
+        (Los efectos se pueden usar pero no es recomendable hacerlo para hacer peticiones HTTP)
+        Con esto al crear la instancia del servicio se ejecuta este efecto, los efecto por defecto segun lo que le pasemos sabe 
+        cuales son sus dependencias 
+
+        Aqui solo guardamos en el LocalStorage (Pero no le estamos diciendo que Lea los datos de ahi para mantener los datos)
+        tratemos de que los efectos solo realizen una tarea 
+    */
+   saveToLocalStorage = effect(() => {
+    // Guardamos en el LocalStorage, en la Key "characters" va a estar almacenado el arreglo y el VALUE tiene que ser String
+    // pero le estamos mandando este arreglo asi que lo Serializamos
+        localStorage.setItem('characters', JSON.stringify(this.characters()));
+   });
+
+
 }
