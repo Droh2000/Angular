fb5a829ac0343ead428e92b40083ec48db2e8840
gomezfarm <gomez.farm@outlook.com>
2025-04-19 11:22:56 -0600

    Componentes y Rutas Hijas para mostrar el contenido dentro del componente Padre

--- 04-gifs-app/src/app/app.routes.ts
+++ 04-gifs-app/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    // Si especificamos la propiedad de "component:" esto hace que el elemento se carge normal sin carga perezosa
+    // y va a ser parte del boundle principal de la aplicacion, pero si queremos que solo sea cargado bajo demanda hasta
+    // que el usuario entre a este componente entonces usamos "loadComponent", la funcion que recibe seri el Lazy Load del componente
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+      //.then(c => c.DashboardPageComponent) -> Esto es una forma de usarlo pero para evitar la promesa agregamos la palabra DEFAULT en la clase del componente
+
+    // Rutas Hijas
+    // Hacer que las nuevas rutas salgan dentro de la ruta padre, Ahora seria: Ruta_Padre/Ruta_Hija
+    children: [
+      // Si al crear las rutas con sus componentes y usarlas nos manda a la pagina de "dashboard" tenemos que cerrar y volver a ejecutar la aplicacion
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page.component'),
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page.component'),
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
+  },
+  // Cualquier otra ruta que no sea de las definidas arriba manda al usuario al Dashboard
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
---
+++ 04-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,14 @@
+// Este archivo lo creamos haciendo click derecho en la carpeta Pages y seleccionando la opcion de Angular Schematics Generate File
+// Este seria nunestro Primer Componente
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard-page',
+  imports: [RouterOutlet],
+  templateUrl: './dashboard-page.component.html',
+  // Angular ya por defecto con esta propiedad en True, con esto nuestros componentes son modulos, porque las importaciones de arriban
+  // hacen que el componente sepa cuales son los objetos que nesecita para construirse y poder trabajar
+  //  standalone: true
+})
+export default class DashboardPageComponent { }
