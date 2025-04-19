383173e49fe37bcee1a2459eb85da336226163bb
gomezfarm <gomez.farm@outlook.com>
2025-04-19 13:02:29 -0600

    Angular Environtments y Path Alias

--- 04-gifs-app/src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ 04-gifs-app/src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { environment } from '@environtments/environment';
+//import { environment } from '../../../../../environtments/environment';
 
 @Component({
   selector: 'app-gifs-side-menu-header',
@@ -6,4 +8,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './side-menu-header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GifsSideMenuHeaderComponent { }
+export class GifsSideMenuHeaderComponent {
+  // Cambiar el contenido del HTML en base a las propiedades que especificamos en las variables de entorno
+  envs = environment;
+}
