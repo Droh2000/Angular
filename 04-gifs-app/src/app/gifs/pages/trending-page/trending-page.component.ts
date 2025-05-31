36d2ae46a08f2d0083a0ab9f3408a935a42261df
gomezfarm <gomez.farm@outlook.com>
2025-05-31 16:17:44 -0600

    viewChild - Tomar referencias del template - detectar cuando hacemos scroll en el DIV

--- 04-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 04-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { ListComponent } from '../../components/list/list.component';
 import { GifService } from '../../services/gifs.service';
 
@@ -26,6 +26,20 @@ export default class TrendingPageComponent {
   // Aqui metemos el servicio de nuestra peticion GET ("inject" es el que usamos para injectar dependencias)
   // Con esto angular verifica si hay una instancia del "GifService" si ya la hay la injecta aqui,si no existe la crea
   GifService = inject(GifService);
+
+  // Tomamos la referencia al HTML que pusimos en el "component" con "#", tenemos dos elementos para tomar referencias a piezas del HTML
+  // viewChild es solo para un elemento, y el viewChildren es para cuando tenemos varios elementos, entre comillas le pasamos el selector
+  // que creamos
+  scrollDivRef = viewChild<ElementRef>('groupDiv');
+
+  // Esto se ejecutara cada vez que hagamos Scroll
+  onScroll( event: Event ){
+    // "nativeElement" es para obtener el elemento HTML, se le pone el signo de ? porque en el momento que se crea el componente
+    // todavia no existe el HTML
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+
+
+  }
 
   // Mostrar los Gifs en pantalla
   // Podemos implementarlos de varias formas (En este caso solo llamaremos el Service de arriba en el HTML)
