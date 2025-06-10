d21bd48c7dfd1e8df8a4bf7a7218419b5323f6b0
gomezfarm <gomez.farm@outlook.com>
2025-06-10 11:56:17 -0600

    Menu Superior con Condicion en el CSS para colocar una clase en la opcion del menu activo

--- 05-country-app/src/app/country/layouts/CountryLayout/CountryLayout.component.ts
+++ 05-country-app/src/app/country/layouts/CountryLayout/CountryLayout.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from "../../components/top-menu/top-menu.component";
 
 @Component({
   selector: 'app-country-layout',
   // Lo que lo hace especial es que internamente vamos a tener el RouterOutlet
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopMenuComponent],
   templateUrl: './CountryLayout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
