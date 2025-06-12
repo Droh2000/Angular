c263cb0f2c2322494eab47d65504ee03afa6d759
gomezfarm <gomez.farm@outlook.com>
2025-06-12 17:16:47 -0600

    RxResource - Trabajar directamente con Observables sin usar Promesas

--- 05-country-app/src/app/country/pages/by-country-page/by-country-page.component.ts
+++ 05-country-app/src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
 import { CountryService } from '../../services/country.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-by-country',
@@ -15,7 +16,8 @@ export class ByCountryPageComponent {
 
   query = signal("");
 
-  countryResource = resource({
+  // Esto es como funciona con promesas
+  /*countryResource = resource({
     request: () => ({
       query: this.query()
     }),
@@ -25,5 +27,21 @@ export class ByCountryPageComponent {
         this.countryService.searchByCountry( request.query )
       );
     }
+  });*/
+
+  // Implementacion con el "RxResource"
+  countryResource = rxResource({
+    request: () => ({
+      query: this.query()
+    }),
+    loader: ({ request }) => {
+      // Si el Texto que se ingreso fue vacio pero como estamos obligados a regresar un Observable
+      // para que no nos de error usamos el RXJS de "of" que nos permite regresar un observable basado en lo que le pasemos
+      if( !request.query ) return  of([]);
+
+      // aqui podemos mandar a llamar directamente el observable
+      return this.countryService.searchByCountry( request.query )
+    }
   });
+
 }
