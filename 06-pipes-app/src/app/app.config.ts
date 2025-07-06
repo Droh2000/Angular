895ecfb6878ce72285ef6c14020a3f721f6ec60f
gomezfarm <gomez.farm@outlook.com>
2025-07-06 13:02:12 -0600

    Cambiar de Idiomas

--- 06-pipes-app/src/app/app.config.ts
+++ 06-pipes-app/src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+// Tomamos los idiomas que nos interesa para despues hacer que el usario cambie segun lo que quiera
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+
+// Tenemos que registrar el LOCAL (Lo que se usa como referencia para inyectar el idioma que queremos)
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // Aqui definimos que tipo de idioma es el que la aplicacion va a seguir
+    {
+      provide: LOCALE_ID,
+      useValue:  'es', // Este es el valor que se inyectara cuando se usa el LocalID
+    }
+  ]
 };
