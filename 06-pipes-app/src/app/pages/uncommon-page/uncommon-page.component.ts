b16bf0a3fbdfbc21280dcffbae5638dd6287b1d2
gomezfarm <gomez.farm@outlook.com>
2025-07-06 17:08:40 -0600

    i18nSelectPipe - Para cambiar contenido segun el genero de la persona involucrada

--- 06-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 06-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Juan',
+  gender: 'male',
+  age: 39,
+  address: 'Ottawa, Canada'
+}
+
+const client2 = {
+  name: 'Juana',
+  gender: 'female',
+  age: 38,
+  address: 'Nigeria'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+  // i18n Select -> Este es el Pipe que vamos a usar
+  // Signal para mantener un cliente
+  client = signal(client1);
+
+  // El pipe funciona con un Objeto Mapa que nos permite cambiar contenido de forma dinamica deacuerdo a una condicion
+  invitationMap = {
+    // Ponemos segun el genero la palabra que tenemos que cambiar
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  // Toggle para cambiar de cliente
+  changeClient(){
+    if( this.client() === client1 ){
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+}
