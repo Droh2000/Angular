f966a1345a05ebcdf5fb1081a96907de90a8fa55
gomezfarm <gomez.farm@outlook.com>
2025-07-06 12:36:37 -0600

    Date Pipe y usoEffect en Angular

--- 06-pipes-app/src/app/pages/basic-pages/basic-pages.component.ts
+++ 06-pipes-app/src/app/pages/basic-pages/basic-pages.component.ts
@@ -1,12 +1,13 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-pages',
   imports: [
     LowerCasePipe,
     UpperCasePipe,
-    TitleCasePipe
+    TitleCasePipe,
+    DatePipe,
   ],
   templateUrl: './basic-pages.component.html',
 })
@@ -15,5 +16,24 @@ export default class BasicPagesComponent {
   nameLower = signal('juan');
   nameUpper = signal('JUAN');
   fullName = signal('jUaN ORtiz');
+
+  // Esta Signal contiene la fecha actual, esto por si solo no se va a actualizar
+  customDate = signal( new Date() );
+
+  // Para poder ver los cambios en tiempo real y que se este actualizando la fecha conforme avanza el tiempo
+  // para eso vamos a usar un efecto de Angular
+  //  CUIDADO CON USAR ESTO porque si no limpiamos el Efecto cada vez que salgamos este seguira ejectuando y cuando entremos volvera a crearse
+  //  otro a parte del que ya estaba creado, creando muchos en conjunto, para limpiarlo en la funcion recibimos ya un argumento
+  tickingDateEffect = effect((onCleanup) => {
+    // Actualizamos por medio de un intervalo actualizando cada segundo
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 3000);
+
+    onCleanup(() => {
+      // Esto es lo que se va a ejecutar cuando se destruya el efecto
+      clearInterval(interval);
+    });
+  });
 
 }
