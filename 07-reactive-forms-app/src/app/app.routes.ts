163ed163452d6aaef0e4b59261f553a536e59076
gomezfarm <gomez.farm@outlook.com>
2025-07-20 17:14:09 -0600

    Implementacion de las rutas y el codigo HTML

--- 07-reactive-forms-app/src/app/app.routes.ts
+++ 07-reactive-forms-app/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Aqui vamos a tener todas las rutas que la aplicacion va a tener
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.routes').then(
+      // Como en el "reactive.routes" no estamos usando una exportacion por defecto asi que colocamos el then
+      // le pusimos "m" de modulo
+      (m) => m.reactiveRoutes
+    ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'), // Aqui si tenemos exportacion por defecto
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes').then(
+      (m) => m.countryRoutes
+    ),
+  },
+  {
+    // Cualquier otro path no reconocido nos mandara a "reactive" que como internamente tiene una ruta padre "path:''"
+    // entonces entrara al "children" que al no haber ninguno que coincida con el path vacio, entonces caer en el path vacio
+    // por lo que caera en el comodin y nos redireccinara al "basic"
+    path: '**',
+    redirectTo: 'reactive',
+  }
+];
--- 07-reactive-forms-app/src/app/shared/components/side-menu/side-menu.component.ts
+++ 07-reactive-forms-app/src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuItem {
+  title: string,
+  route: string
+}
+
+// Como tenemos una ruta padre y queremos la primera en la cual tenemos el "path=''"
+// y obtenemos el children, si no hay nada mandamos un [] (Esto es para asegurarnos que siempre vamos a tener rutas y no vamos a caer en un undefined)
+const reactiveItems = reactiveRoutes[0].children ?? [];
+
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent { }
+export class SideMenuComponent {
+  reactiveMenu: MenuItem[] = reactiveItems
+  // Como tenemos un problema del undefined por la parte de reactiveRoutes "path:**"
+  .filter((item) => item.path !== '**')
+  .map((item) => ({
+    route: `reactive/${item.path}`, // Le agregamos el segmento de la ruta porque solo obtenemos el nombre
+    title: `${item.title}`, // Lo ponemos asi para que sea de tipo String
+  }));
+
+  authMenu: MenuItem[] = [{
+    title: 'Registro',
+    route: './auth'
+  }];
+
+  countryMenu: MenuItem[] = [{
+    title: 'Paises',
+    route: './country'
+  }];
+}
