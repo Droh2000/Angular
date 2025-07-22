e9cbe92e028c8cf8e51a1b15e474186b9fd2c1eb
gomezfarm <gomez.farm@outlook.com>
2025-07-22 17:12:18 -0600

    Nuevo formulario Switches, Checks y Radio buttons - Agregar validaciones

--- 07-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 07-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,33 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [ JsonPipe ],
+  imports: [ JsonPipe,
+             ReactiveFormsModule, // Para asignar referencias al HTML
+  ],
   templateUrl: './switches-page.component.html',
 })
-export class SwitchesPageComponent { }
+export class SwitchesPageComponent {
+
+  // Aunque lo podemos crear manualmente es mas facil trabajar con el Fontbuilder
+  private fb = inject(FormBuilder);
+
+  // Tener acceso a las funciones helpers que hemos creado
+  formUtils =  FormUtils;
+
+  // Creamos el formulario con los campos que tiene y validaciones
+  myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],// El valor por defecto seleccionado es: 'M' y sera obligatorio (Seleccionando uno de los dos que hay)
+    wantNotifications: [true], // El valor por defecto sera seleccionado
+    termAndConditions: [false, Validators.requiredTrue], // Obligamos que sea seleccionado
+  });
+
+  onSubmit(){
+    // Repasa todos los campos y si no esta uno correcto nos saldara que tiene errores
+    this.myForm.markAllAsTouched();
+  }
+
+}
