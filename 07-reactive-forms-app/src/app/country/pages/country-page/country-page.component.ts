9aaaf44d55f21d3cbf7ac7ece7aee1baa618c50b
gomezfarm <gomez.farm@outlook.com>
2025-08-11 17:04:07 -0600

    Primer selector controlado - Obtener los  Continentes

--- 07-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
+++ 07-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interfaces';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ ReactiveFormsModule, JsonPipe ],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
+
+  // Llenamos la informacion del primer selector
+  regions = signal( this.countryService.regions );
+  // Obtenemos la data de los otros selectores y no le pasamos la data porque estos se rellenaran segun la opcion del selector elegida
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  // Creamos el formulario con los campos que tendra
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+}
